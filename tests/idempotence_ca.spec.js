e0845afb140151638c60af47205143b62004993b
Asad Hasan <asadhasan@Asads-MacBook-Pro-2.local>
2024-03-12 18:29:39 -0500

    Formatting.

--- tests/idempotence_ca.spec.js
+++ tests/idempotence_ca.spec.js
@@ -1,12 +1,25 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-const type_delay = 20;
+const delay_max = 100;
+const delay_min = 10;
 
-async function typeWithDelay(page, selector, text, delay) {
+function getRandomInt(min, max) {
+  // Ensure min <= max
+  if (min > max) {
+    [min, max] = [max, min]; // Swap values if min is greater than max
+  }
+
+  // Utilize Math.floor and Math.random to get a random integer within the range
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function typeWithDelay(page, selector, text) {
   for (const char of text) {
     await page.type(selector, char);
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    await new Promise((resolve) =>
+      setTimeout(resolve, getRandomInt(delay_min, delay_max))
+    );
   }
 }
 
@@ -16,31 +29,32 @@ test.beforeEach(async ({ page }) => {
   await typeWithDelay(
     page,
     'input[id="email-address"]',
-    "garnetthudson@funk.biz",
-    type_delay
+    "garnetthudson@funk.biz"
   );
-  await typeWithDelay(
-    page,
-    'input[id="password"]',
-    "RPBUK%KE{ fq9zq;er<H",
-    type_delay
-  );
-  await page.click(`button[id="form-submit"]`);
+  await typeWithDelay(page, 'input[id="password"]', "RPBUK%KE{ fq9zq;er<H");
+  await page.click(`button[id="form-submit"]`, {
+    delay: getRandomInt(1000, 3000),
+  });
   await page.waitForSelector('ul[role="list"]');
 });
 
 test("IC-2 Send Connection Request", async ({ page }) => {
-  await page.click(`ul[role="list"] li:nth-child(2) a`);
+  await page.click(`ul[role="list"] li:nth-child(2) a`, {
+    delay: getRandomInt(1000, 3000),
+  });
   await page.waitForSelector(`main button[type="button"]`);
-  await page.click(`main button[type="button"]`);
+  await page.click(`main button[type="button"]`, {
+    delay: getRandomInt(1000, 3000),
+  });
   await page.waitForSelector(`input[id="email-address"]`);
   await typeWithDelay(
     page,
     'input[id="email-address"]',
-    "holliekris@ernser.name",
-    type_delay
+    "holliekris@ernser.name"
   );
 
-  await page.click(`.text-right button[id="form-submit"]`);
+  await page.click(`.text-right button[id="form-submit"]`, {
+    delay: getRandomInt(1000, 3000),
+  });
   await page.waitForTimeout(5000);
 });
